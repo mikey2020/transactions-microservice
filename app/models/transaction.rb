c819b6839184386b73491f0a5f3f52353925db38
Supermike <superdafe@gmail.com>
2022-03-22 15:22:41 +0100

    update output calc

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,13 @@
 class Transaction < ApplicationRecord
+  before_save :output_calc #{ output = input * 2 }
+
   validates :customer_id, presence: true
   validates :date_of_transaction, presence: true
   validates :input, presence: true
-  validates :output,presence: true
+  #validates :output,presence: true
   validates :state, presence: true
+
+  def output_calc
+    self.output = self.input.to_i * 2
+  end
 end
